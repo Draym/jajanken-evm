1f9346a8dc6073e1ecac899b45c15067dd9c3464
Draym <kevin.draym@gmail.com>
2021-09-13 19:18:17 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jajanken-evm
---
+++ test/GameFlow.test.js
@@ -0,0 +1,12 @@
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('JajankenGame', ([owner, player1, player2]) => {
+    before(async () => {
+    })
+
+    describe('Player Join Game', async () => {
+    })
+})
